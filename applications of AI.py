98c02b2d7d4970ea35cc939e8c274b5ed223563a
Nicholoson-V <109790233+Nicholoson-V@users.noreply.github.com>
2022-08-28 20:19:45 +0530

    Add files via upload

---
+++ applications of AI.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: NICK
+
+"""
+
+# 5 applications of AI in our dailylife.
+
+#Streaming Apps
+#Navigation
+#Games
+#Self-driving Vehicles
+#Facial Recognition 
+
