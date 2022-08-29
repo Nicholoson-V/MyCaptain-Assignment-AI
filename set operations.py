90d7739ec62bf5a11b890fbf547347431c628669
Nicholoson-V <109790233+Nicholoson-V@users.noreply.github.com>
2022-08-29 11:44:56 +0530

    Add files via upload

---
+++ set operations.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author: NICK
+"""
+A = {0, 2, 4, 6, 8}
+B = {1, 3, 5, 7, 9}
+
+print("Union :", A | B)
+
+print("Intersection :", A & B)
+
+print("Difference :", A - B)
+
+print("Symmetric difference :", A ^ B)
